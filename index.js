073c4e7181c88b72fb5699e0c0b5380a63954934
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-11-22 22:32:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: 'https://foodpoorna2.netlify.app',
+  origin: 'https://recipe-app-front-end-bswl.vercel.app',
   credentials: true
 }));
 app.use(express.json());
